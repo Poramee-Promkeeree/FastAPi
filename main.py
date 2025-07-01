51b7011448b8fec28a9ec02006506c968a51bb87
Poramee Promkeeree <6510110267@email.psu.ac.th>
2025-07-01 14:47:18 +0700

    แก้ไข main.py ทดลองการใช้ post

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel): 
+    name: str
+    price: float
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
+@app.get("/hello")
+def hello_world():
+    return { "message": "Hello World 3" }
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/items")
+def create_item(item: Item): 
+    print(item.name, item.price)
+    return { "request body": item }

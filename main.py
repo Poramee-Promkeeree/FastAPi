99b6f27668aaf955439185fcecadc0d5c959303c
Poramee Promkeeree <6510110267@email.psu.ac.th>
2025-07-01 15:39:19 +0700

    แยกไฟล์

--- main.py
+++ main.py
@@ -1,59 +1,14 @@
 from fastapi import FastAPI, HTTPException, Depends
-from pydantic import BaseModel
-from typing import List, Optional
+from typing import List
+from sqlalchemy.orm import Session
+from .database import Base, engine, get_db
+from .models import ItemDB
+from .schemas import ItemCreate, ItemResponse
 
-from sqlalchemy import create_engine, Column, Integer, String, Float, Text
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
-
-DATABASE_URL = "sqlite:///./test.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-
-
-class ItemDB(Base):
-    __tablename__ = "items"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    description = Column(Text, nullable=True)
-    price = Column(Float, nullable=False)
-    tax = Column(Float, nullable=True)
 
 Base.metadata.create_all(bind=engine)
 
-
-
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-
-
-class ItemCreate(Item):
-    pass
-
-
-class ItemResponse(Item):
-    id: int
-
-    class Config:
-        from_attributes = True
-
-
 app = FastAPI()
-
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 @app.post("/items/", response_model=ItemResponse)
@@ -65,11 +20,9 @@ async def create_item(item: ItemCreate, db: Session = Depends(get_db)):
     return db_item
 
 
-
 @app.get("/items/", response_model=List[ItemResponse])
 async def read_items(db: Session = Depends(get_db)):
     return db.query(ItemDB).all()
-
 
 
 @app.get("/items/{item_id}", response_model=ItemResponse)
@@ -78,7 +31,6 @@ async def read_item(item_id: int, db: Session = Depends(get_db)):
     if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return db_item
-
 
 
 @app.put("/items/{item_id}", response_model=ItemResponse)
